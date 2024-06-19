5fc669c659178895c38a2d9d9b75ef00cb2d62ed
iMnOtNeO19 <98492123+NeoFox19@users.noreply.github.com>
2024-06-18 21:04:21 -0600

    Update ping.js

--- bot/discord/commands/ping.js
+++ bot/discord/commands/ping.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
     const embed = new Discord.MessageEmbed()
         .setColor("RED")
-        .setTitle("DanBot Hosting - Ping")
+        .setTitle("LylaNodesStats - Ping")
         .setDescription(
             `Bot Latency: ${Date.now() - message.createdTimestamp}ms\nAPI Latency: ${Math.round(client.ws.ping)}ms`
         )
